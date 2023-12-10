f3c1e6812978414add773d1cfffcccfeec981204
alic3 <alic3dev@gmail.com>
2023-12-10 15:59:03 -0500

    Add Escape key listener to `DialogContainer`

--- src/components/layout/Dialogs/DialogContainer.tsx
+++ src/components/layout/Dialogs/DialogContainer.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 import styles from './DialogContainer.module.scss'
 
 export function DialogContainer({
@@ -7,22 +9,37 @@ export function DialogContainer({
   dialogs: JSX.Element[]
   setDialogs: React.Dispatch<React.SetStateAction<JSX.Element[]>>
 }): JSX.Element {
+  const closeDialogs = React.useCallback(() => setDialogs([]), [setDialogs])
+
+  React.useEffect(() => {
+    const escKeydownListener = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        event.preventDefault()
+        event.stopPropagation()
+
+        closeDialogs()
+      }
+    }
+
+    window.addEventListener('keydown', escKeydownListener)
+
+    return () => window.removeEventListener('keydown', escKeydownListener)
+  }, [closeDialogs])
+
   return (
-    <>
-      <div
-        className={`${styles['dialog-container']} ${
-          dialogs.length ? styles.active : ''
-        }`}
-        onClick={(event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-          if (event.target !== event.currentTarget) return
+    <div
+      className={`${styles['dialog-container']} ${
+        dialogs.length ? styles.active : ''
+      }`}
+      onClick={(event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        if (event.target !== event.currentTarget) return
 
-          // TODO: Should we only remove the topmost dialog (pop?) or keep as is?
+        // TODO: Should we only remove the topmost dialog (pop?) or keep as is?
 
-          setDialogs([])
-        }}
-      >
-        {dialogs}
-      </div>
-    </>
+        closeDialogs()
+      }}
+    >
+      {dialogs}
+    </div>
   )
 }
