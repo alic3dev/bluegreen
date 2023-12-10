94529439e66f177a4fe6882344adf009d1d70258
alic3 <alic3dev@gmail.com>
2023-12-09 20:55:49 -0500

    Add general utilities module

---
+++ src/utils/general.ts
@@ -0,0 +1,30 @@
+export interface Position {
+  bar: number
+  beat: number
+  repeated: number
+}
+
+export interface BarData {
+  notes: number[][][]
+  repeat: number
+}
+
+export const generateBeat = (
+  frequencies: number[],
+  polyphony: number = 2,
+): number[][] =>
+  new Array<number>(polyphony)
+    .fill(-1)
+    .map((): number[] => [Math.floor(Math.random() * frequencies.length)])
+
+// TODO: Don't generate random - Save states?
+export const generateBar = (
+  frequencies: number[],
+  beats: number = 4,
+  polyphony?: number,
+): BarData => ({
+  notes: new Array<number[]>(beats)
+    .fill([])
+    .map((): number[][] => generateBeat(frequencies, polyphony)),
+  repeat: 0,
+})
