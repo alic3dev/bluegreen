0994613734640529e3a659f5355bc8ff454ba3a6
alic3 <alic3dev@gmail.com>
2024-03-04 15:06:36 -0500

    Navigation screen updates (Design mainly)

--- src/components/apps/NavigationApp.tsx
+++ src/components/apps/NavigationApp.tsx
@@ -1,15 +1,39 @@
 import { Link } from 'react-router-dom'
 
+import { Alic3 } from '../decorative/Alic3'
+
 import styles from './NavigationApp.module.scss'
 
 export function NavigationApp() {
   return (
     <div className={styles.navigation}>
-      <Link to="/circle">Circle</Link>
-      <Link to="/eye-gen">Eye Gen</Link>
-      <Link to="/noise">Noise</Link>
-      <Link to="/visdio">Visdio</Link>
-      <Link to="/zer0">ゼロ</Link>
+      <div className={styles['alic3-wrapper']}>
+        <Alic3 />
+      </div>
+
+      <h1 className={styles.title}>
+        <Link to="/">
+          <span className={styles.blue}>BLUE</span>
+          <span className={styles.green}>GREEN</span>
+        </Link>
+      </h1>
+
+      <div className={styles['navigation-wrapper']}>
+        <div className={styles['navigation-section']}>
+          <div className={styles['navigation-container']}>
+            <h2>DAW</h2>
+            <Link to="/zer0">ゼロ</Link>
+          </div>
+
+          <div className={styles['navigation-container']}>
+            <h2>Misc</h2>
+            <Link to="/circle">Circle</Link>
+            <Link to="/eye-gen">Eye Gen</Link>
+            <Link to="/noise">Noise</Link>
+            <Link to="/visdio">Visdio</Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
