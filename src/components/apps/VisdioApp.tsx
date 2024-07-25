dbcf8953418bbc61d0d8660b97a13f386c1426c0
alic3 <alic3dev@gmail.com>
2024-07-25 03:40:25 -0400

    Add squares to Visdio

--- src/components/apps/VisdioApp.tsx
+++ src/components/apps/VisdioApp.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { mat4 } from 'gl-matrix'
 
+import * as twgl from 'twgl.js'
+
 import { Size2D } from '../../utils/visual'
 
 import styles from './VisdioApp.module.scss'
@@ -201,101 +203,201 @@ export function VisdioApp(): JSX.Element {
   const canvasRef = React.useRef<HTMLCanvasElement>(null)
 
   React.useEffect((): (() => void) | void => {
+    let animationFrameHandle: number
     if (!canvasRef.current) return
 
-    const gl: WebGL2RenderingContext | null =
-      canvasRef.current.getContext('webgl2')
+    const gl: WebGLRenderingContext | null =
+      canvasRef.current.getContext('webgl')
 
-    let animationFrameHandle: number
+    if (!gl) return
 
-    function animationFrame(): void {
-      if (!gl) return
-
-      // Set clear color to black, fully opaque
-      gl.clearColor(0.0, 0.0, 0.0, 1.0)
-
-      // Clear the color buffer with specified clear color
-      gl.clear(gl.COLOR_BUFFER_BIT)
-
-      // Vertex shader program
-      const vsSource: string = `
-        attribute vec4 aVertexPosition;
-        uniform mat4 uModelViewMatrix;
-        uniform mat4 uProjectionMatrix;
-        void main() {
-          gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-        }
-      `
-
-      const fsSource: string = `
-        void main() {
-          gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-        }
-      `
-
-      const vertexShader: WebGLShader = loadShader({
-        gl,
-        type: gl.VERTEX_SHADER,
-        source: vsSource,
-      })
-      const fragmentShader: WebGLShader = loadShader({
-        gl,
-        type: gl.FRAGMENT_SHADER,
-        source: fsSource,
-      })
-
-      // Create the shader program
-      const shaderProgram: WebGLProgram | null = gl.createProgram()
-      if (!shaderProgram) throw new Error('Unable to create shader program')
-
-      gl.attachShader(shaderProgram, vertexShader)
-      gl.attachShader(shaderProgram, fragmentShader)
-      gl.linkProgram(shaderProgram)
-
-      // If creating the shader program failed, alert
-      if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-        throw new Error(
-          `Unable to initialize the shader program: ${gl.getProgramInfoLog(
-            shaderProgram,
-          )}`,
-        )
-      }
-
-      // Collect all the info needed to use the shader program.
-      // Look up which attribute our shader program is using
-      // for aVertexPosition and look up uniform locations.
-      const programInfo: ProgramInfo = {
-        program: shaderProgram,
-        attribLocations: {
-          vertexPosition: gl.getAttribLocation(
-            shaderProgram,
-            'aVertexPosition',
-          ),
-        },
-        uniformLocations: {
-          projectionMatrix: gl.getUniformLocation(
-            shaderProgram,
-            'uProjectionMatrix',
-          ),
-          modelViewMatrix: gl.getUniformLocation(
-            shaderProgram,
-            'uModelViewMatrix',
-          ),
-        },
-      }
-
-      // Here's where we call the routine that builds all the
-      // objects we'll be drawing.
-      const buffers: Buffers = initBuffers(gl)
-
-      // Draw the scene
-      drawScene({ gl, programInfo, buffers })
-
-      animationFrameHandle = window.requestAnimationFrame(animationFrame)
+    const ext = gl.getExtension('ANGLE_instanced_arrays')
+    if (!ext) {
+      return alert('need ANGLE_instanced_arrays')
     }
-    animationFrameHandle = window.requestAnimationFrame(animationFrame)
+    twgl.addExtensionsToContext(gl)
 
-    return (): void => window.cancelAnimationFrame(animationFrameHandle)
+    const vs = `
+      attribute float id;
+      attribute vec4 position;
+      attribute vec2 texcoord;
+      
+      uniform float time;
+      
+      varying vec2 v_texcoord;
+      varying vec4 v_color;
+      
+      void main() {
+        float o = id + time;
+        gl_Position = position + vec4(
+            vec2(
+                fract(o * 0.1373),
+                fract(o * 0.5127)) * 2.0 - 1.0,
+            0, 0);
+            
+        v_texcoord = texcoord;
+        v_color = vec4(fract(vec3(id) * vec3(0.127, 0.373, 0.513)), 1);
+      }
+    `
+
+    const fs = `
+      precision mediump float;
+      varying vec2 v_texcoord;
+      varying vec4 v_color;
+      
+      float circle(in vec2 st, in float radius) {
+        vec2 dist = st - vec2(0.5);
+        return 1.0 - smoothstep(
+          radius - (radius * 0.01),
+          radius +(radius * 0.01),
+          dot(dist, dist) * 4.0);
+      }
+      
+      void main() {
+        if (circle(v_texcoord, 1.0) < 0.5) {
+          discard;
+        }
+        gl_FragColor = v_color;
+      }
+    `
+
+    // compile shaders, link program, look up locations
+    const programInfo = twgl.createProgramInfo(gl, [vs, fs])
+
+    const maxCount = 250000
+    const ids = new Float32Array(maxCount)
+    for (let i = 0; i < ids.length; ++i) {
+      ids[i] = i
+    }
+    const x = (16 / 300) * 2
+    const y = (16 / 150) * 2
+
+    const bufferInfo = twgl.createBufferInfoFromArrays(gl, {
+      position: {
+        numComponents: 2,
+        data: [-x, -y, x, -y, -x, y, -x, y, x, -y, x, y],
+      },
+      texcoord: [0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0],
+      id: {
+        numComponents: 1,
+        data: ids,
+        divisor: 1,
+      },
+    })
+    twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo)
+
+    const count = 10
+
+    const maxHistory = 60
+    const fpsHistory = new Array(maxHistory).fill(0)
+    let historyNdx = 0
+
+    let then = 0
+    function render(now: DOMHighResTimeStamp) {
+      const deltaTime = now - then
+      then = now
+
+      fpsHistory[historyNdx] = deltaTime
+      historyNdx = (historyNdx + 1) % maxHistory
+
+      gl!.useProgram(programInfo.program)
+      twgl.setUniforms(programInfo, { time: now * 0.001 })
+      ext!.drawArraysInstancedANGLE(gl!.TRIANGLES, 0, 6, count)
+      animationFrameHandle = requestAnimationFrame(render)
+    }
+    animationFrameHandle = requestAnimationFrame(render)
+    return () => window.cancelAnimationFrame(animationFrameHandle)
+
+    // // let animationFrameHandle: number
+
+    // // function animationFrame(): void {
+    // //   if (!gl) return
+
+    // //   // Set clear color to black, fully opaque
+    // //   gl.clearColor(0.0, 0.0, 0.0, 1.0)
+
+    // //   // Clear the color buffer with specified clear color
+    // //   gl.clear(gl.COLOR_BUFFER_BIT)
+
+    // //   // Vertex shader program
+    // //   const vsSource: string = `
+    // //     attribute vec4 aVertexPosition;
+    // //     uniform mat4 uModelViewMatrix;
+    // //     uniform mat4 uProjectionMatrix;
+    // //     void main() {
+    // //       gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+    // //     }
+    // //   `
+
+    // //   const fsSource: string = `
+    // //     void main() {
+    // //       gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    // //     }
+    // //   `
+
+    // //   const vertexShader: WebGLShader = loadShader({
+    // //     gl,
+    // //     type: gl.VERTEX_SHADER,
+    // //     source: vsSource,
+    // //   })
+    // //   const fragmentShader: WebGLShader = loadShader({
+    // //     gl,
+    // //     type: gl.FRAGMENT_SHADER,
+    // //     source: fsSource,
+    // //   })
+
+    // //   // Create the shader program
+    // //   const shaderProgram: WebGLProgram | null = gl.createProgram()
+    // //   if (!shaderProgram) throw new Error('Unable to create shader program')
+
+    // //   gl.attachShader(shaderProgram, vertexShader)
+    // //   gl.attachShader(shaderProgram, fragmentShader)
+    // //   gl.linkProgram(shaderProgram)
+
+    // //   // If creating the shader program failed, alert
+    // //   if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    // //     throw new Error(
+    // //       `Unable to initialize the shader program: ${gl.getProgramInfoLog(
+    // //         shaderProgram,
+    // //       )}`,
+    // //     )
+    // //   }
+
+    // //   // Collect all the info needed to use the shader program.
+    // //   // Look up which attribute our shader program is using
+    // //   // for aVertexPosition and look up uniform locations.
+    // //   const programInfo: ProgramInfo = {
+    // //     program: shaderProgram,
+    // //     attribLocations: {
+    // //       vertexPosition: gl.getAttribLocation(
+    // //         shaderProgram,
+    // //         'aVertexPosition',
+    // //       ),
+    // //     },
+    // //     uniformLocations: {
+    // //       projectionMatrix: gl.getUniformLocation(
+    // //         shaderProgram,
+    // //         'uProjectionMatrix',
+    // //       ),
+    // //       modelViewMatrix: gl.getUniformLocation(
+    // //         shaderProgram,
+    // //         'uModelViewMatrix',
+    // //       ),
+    // //     },
+    // //   }
+
+    // //   // Here's where we call the routine that builds all the
+    // //   // objects we'll be drawing.
+    // //   const buffers: Buffers = initBuffers(gl)
+
+    // //   // Draw the scene
+    // //   drawScene({ gl, programInfo, buffers })
+
+    // //   animationFrameHandle = window.requestAnimationFrame(animationFrame)
+    // // }
+    // // animationFrameHandle = window.requestAnimationFrame(animationFrame)
+
+    // return (): void => window.cancelAnimationFrame(animationFrameHandle)
   }, [])
 
   return (
